1caabcb91300936da21752e0b0c2827ebe4b4e96
m124578n <m23568n@gmail.com>
2023-10-11 10:43:04 +0800

    add category in store models

--- OrderMeal/store/models.py
+++ OrderMeal/store/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Store(models.Model):
+    class Category(models.IntegerChoices):
+        BENTO = 1
+        DRINK = 2
+        OTHER = 3
     name = models.CharField(max_length=255)
     describe = models.CharField(max_length=255)
+    catagory = models.IntegerField(choices=Category.choices)
     note = models.CharField(max_length=255)
 
     class Meta:
